938373bca5b88a0694dcb895993b9e1a15c7d286
Mihail Solovev <m.solovev@geoscan.ru>
2024-09-03 10:13:23 +0300

    Версия 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='tmark',
-    version='0.0.1',
+    version='0.0.2',
     author='OnisOris',
     author_email='onisoris@yandex.ru',
     description='Time marking package in code.',
